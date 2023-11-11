bd81da2803e71b6d536260c9406719d76e52be45
carmen-palma <148655498+carmen-palma@users.noreply.github.com>
2023-11-11 01:20:06 +0100

    cssyjs

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
  // Aquí tu código
+document.addEventListener("DOMContentLoaded", function() {
+    const agregarBtn = 
+document.getElementById("agregar");
+    const lista =
+document.getElementById("lista");
+
+agregarBtn.addEventListener("click", function() {
+    const nuevoElement =
+prompt("Ingrese un nuevo elemento:")
+
+    if (nuevoElement !== null && nuevoElemento.trim() !== "") {
+        const nuevoLi = 
+document.createElement("li");
+        nuevoLi.textContent = nuevoElemento;
+lista.appendChild(nuevoLi);
+        }
+    });
+});
